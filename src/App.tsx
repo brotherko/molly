9264126e62559d054e6ebaf5ddf423f885ce22fd
Marty Ko <marty.ko@kadensacapital.com>
2020-08-28 22:34:08 +0800

    fix: add basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import { AddGameView } from './views/AddGameView';
+import { ResultView } from './views/ResultView';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import React from 'react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Icon, BottomNavigation, BottomNavigationAction, Container, Box, MuiThemeProvider, CssBaseline } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+          <Router>
+            <div className="appContent">
+              <Container>
+                <Switch>
+                  <Route path="/add">
+                    <AddGameView />
+                  </Route>
+                  <Route path="/result">
+                    <ResultView />
+                  </Route>
+                </Switch>
+              </Container>
+            </div>
+            <BottomNavigation>
+              <BottomNavigationAction
+                component={Link}
+                to="/add"
+                label="Add"
+                value="add"
+                icon={<AddBoxIcon />}
+              />
+              <BottomNavigationAction
+                component={Link}
+                to="/result"
+                label="Result"
+                value="result"
+                icon={<AddBoxIcon />}
+              />
+              <BottomNavigationAction
+                component={Link}
+                to="/add"
+                label="Option"
+                value="add"
+                icon={<AddBoxIcon />}
+              />
+            </BottomNavigation>
+          </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
